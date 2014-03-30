a7416bb775ff41e77748b66c00b8b41903ddae04
jpw <pwentzel@gmail.com>
2014-03-30 13:38:38 +0200

    Added SDL2

---
+++ Prog3/GameEngine/GameEngine.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <vector>
+#include "Drawable.h"
+
+class GameEngine{
+public:
+	//static GameEngine getEngineInstance();
+	void render_grid_dots();
+	void render_dot();
+	GameEngine::GameEngine(int width = 640, int height = 480);
+
+	~GameEngine();
+	SDL_Renderer* getRenderer(){ return renderer; }
+
+	void addDrawable(Drawable* d);
+	void renderScene();
+	void graph();
+
+private:
+	std::vector<Drawable*> objects;
+	//static GameEngine instance;
+	bool init();
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	bool ttf_init;
+	int screen_height;
+	int screen_width;
+};
+
