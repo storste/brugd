8c521586e3fa1693f5ef42361329e93f83ee1ef7
krla3102 <storste@gmail.com>
2014-05-18 17:32:34 +0200

    va i hela faaen

--- Prog3/GameClient/Player.cpp
+++ Prog3/GameClient/Player.cpp
@@ -9,26 +9,19 @@ Player::Player(const char* filename, SDL_Renderer* r, const char* name) :Sprite(
 }
 
 Player::Player(){
-
+	std::cout << "player constructor" << std::endl;
 }
 
 
 void Player::update(int dt){
-
 	Sprite::update(dt);
+
+	
 
 	if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
 	{
-		for (auto& o : GameEngine::getEngineInstance()->getObjects()){
-			
-			if (GameEngine::getEngineInstance()->cd(this, o)){
-				std::cout << "player collided with " << o->getName() << std::endl;
-			}
-
-	if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
-	{	
 		Sprite::getAnimation()->setFlip(false);
-		setPosition(getX() + 1, getY());
+		setPosition(getX() + 3, getY());
 	}
 	if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
 	{
