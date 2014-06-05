486bd797f604f6a19db7bb235773d698283de754
golfbag <hasselquist.fabian@gmail.com>
2014-06-05 14:41:19 +0200

    image2

--- Prog3/GameEngine/Image.cpp
+++ Prog3/GameEngine/Image.cpp
@@ -6,50 +6,50 @@ class Image{
 public:
 	Image(SDL_Surface* i = 0);
 	Image(std::string p, bool b);
-	Image(const Image& other);
-	const Image& operator=(const Image& other);
+	Image(const Image& other); 
+	const Image& operator=(const Image& other); 
 	SDL_Surface* getSurface() const { return theImage; }
-	~Image();
+	~Image(); 
 private:
 	SDL_Surface* theImage;
 	bool isAlpha;
 	std::string path;
 };
-
-Image::Image(SDL_Surface* i) : theImage(i){
+//Konstruktor
+Image::Image(SDL_Surface* i) : theImage(i){ 
 	if (theImage)
 		theImage->refcount++;
-	std::cout << "def k: " << theImage->refcount;
+	std::cout << "def konstru: " << theImage->refcount;
 }
-
+//Konstruktor 2
 Image::Image(std::string p, bool b) : path(p), isAlpha(b){
 	theImage = SDL_LoadBMP(path.c_str());
 	//if (theImage)
 	//	theImage->refcount++;
-	std::cout << "sec k ; " << theImage->refcount;
+	std::cout << "sec konstru: " << theImage->refcount;
 }
-
+//Copy konstruktor
 Image::Image(const Image& other) :theImage(other.theImage){
 	if (theImage)
 		theImage->refcount++;
-	std::cout << "copy : " << theImage->refcount;
+	std::cout << "copy konstru: " << theImage->refcount;
 }
-
+//Tilldelningsoperator
 const Image& Image::operator=(const Image& other){
 	if (theImage != other.theImage){
 		if (theImage)
 			SDL_FreeSurface(theImage);
 		theImage = other.theImage;
 		theImage->refcount++;
-		std::cout << "tiil : " << theImage->refcount;
+		std::cout << "tilldel: " << theImage->refcount;
 	}
 	return *this;
 }
-
+//Detruktor
 Image::~Image(){
 	if (theImage){
 		SDL_FreeSurface(theImage);
 		if (theImage->refcount == 0)
-			std::cout << " delete " << theImage->refcount;
+			std::cout << "delete: " << theImage->refcount;
 	}
 }
